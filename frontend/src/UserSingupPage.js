29e7d735f25dbde124b60d3c73371a5bc71dbb5b
tokayOzgur <ozytky@gmail.com>
2022-08-02 23:43:25 +0300

    h2-datbase kurulumu

--- frontend/src/UserSingupPage.js
+++ frontend/src/UserSingupPage.js
@@ -1,37 +1,41 @@
-
+import axios from 'axios';
 import React from 'react'
 
 class UserSingupPage extends React.Component {
 
 
   state = {
-    username: null,
-    agreedClicked:false,
-    displayName:null,
-    password:null,
-    passwordRepeat:null
+    userName: null,
+    agreedClicked: false,
+    displayName: null,
+    password: null,
+    passwordRepeat: null
   }
 
-  // onChangeUsername = event => {
-  //   console.log(event.target.value);
-  //   this.setState({
-  //     username : event.target.value
-  //   });
-  // }
+  onChangeAgreed = event => {
+    console.log(this.state);
+    this.setState({
+      agreedClicked: event.target.checked
+    })
+  }
 
-  // onChangeAgreed= event=>{
-  //   console.log(this.state);
-  //   this.setState({
-  //     agreedClicked:event.target.checked
-  //   })
-  // }
-
-  onChange = event=>{
+  onChange = event => {
     const value = event.target.value;
     const field = event.target.name;
     this.setState({
-      [field]:value
+      [field]: value
     })
+  }
+
+  onClickSingUp = event => {
+    event.preventDefault();
+    const { userName, displayName, password } = this.state;
+    const body = {
+      userName,
+      displayName,
+      password
+    }
+    axios.post('/api/1.0/users/add', body)
   }
 
   render() {
@@ -39,26 +43,24 @@ class UserSingupPage extends React.Component {
       <form>
         <div>
           <label>Username:</label>
-          <input name='username' onChange={this.onChange} />
+          <input name='userName' onChange={this.onChange} />
         </div>
-
         <div>
           <label>Display Name:</label>
-          <input name='displayName' onChange={this.onChange}/>
+          <input name='displayName' onChange={this.onChange} />
         </div>
-
         <div>
           <label>Password:</label>
-          <input type={'password'} name="password" onChange={this.onChange}/>
+          <input type={'password'} name="password" onChange={this.onChange} />
         </div>
         <div>
           <label>Password Repeat:</label>
-          <input type={'password'} name="passwordRepeat" onChange={this.onChange}/>
+          <input type={'password'} name="passwordRepeat" onChange={this.onChange} />
         </div>
         <div>
           <input type={'checkbox'} name="agreedClicked" onChange={this.onChange} /> Agreed
         </div>
-        <button disabled={!this.state.agreedClicked}>Sing Up</button>
+        <button disabled={!this.state.agreedClicked} onClick={this.onClickSingUp}>Sing Up</button>
       </form>
     );
   }
--- ws/src/main/java/com/tokay/ws/user/User.java
+++ ws/src/main/java/com/tokay/ws/user/User.java
@@ -1,9 +1,22 @@
 package com.tokay.ws.user;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "users")
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
 	private String userName;
 
 	private String displayName;
