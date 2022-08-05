ed67e09ba041af1977de02f50febdef7435b68f1
tokayOzgur <ozytky@gmail.com>
2022-08-05 23:31:47 +0300

    Username onaylaması

--- ws/src/main/java/com/tokay/ws/user/UserController.java
+++ ws/src/main/java/com/tokay/ws/user/UserController.java
@@ -1,25 +1,37 @@
 package com.tokay.ws.user;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tokay.ws.error.ApiError;
+import com.tokay.ws.shared.GenericResponse;
+
 @RestController
 @RequestMapping("/api/1.0/users")
 public class UserController {
-	
-	private static final Logger log = LoggerFactory.getLogger(UserController.class);
-	
+
 	@Autowired
 	UserService userService;
-	
+
 	@PostMapping("/add")
-	public void createUser(@RequestBody User user) {
+	public ResponseEntity<?> createUser(@RequestBody User user) {
+		if (user.getUserName() == null || user.getUserName().isEmpty()) {
+			ApiError apiError = new ApiError(400, "Validation error!", "/api/1.0/users");
+			Map<String, String> validationErrors = new HashMap<>();
+			validationErrors.put("username", "Username can not be null");
+			apiError.setValidationErrors(validationErrors);
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
+		}
 		userService.addUser(user);
+		return ResponseEntity.ok(new GenericResponse("user created"));
 	}
-	
+
 }
