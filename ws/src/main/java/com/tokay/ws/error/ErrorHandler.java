2c2d82cddd51d12d842492cd91107602a6c5e445
tokayOzgur <ozytky@gmail.com>
2022-08-13 16:52:53 +0300

    ErrorHandler for All Error Cases

--- ws/src/main/java/com/tokay/ws/error/ErrorHandler.java
+++ ws/src/main/java/com/tokay/ws/error/ErrorHandler.java
@@ -1,9 +1,14 @@
 package com.tokay.ws.error;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.validation.FieldError;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.error.ErrorAttributeOptions.Include;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,12 +26,22 @@ public class ErrorHandler implements ErrorController {
 	private ErrorAttributes errorAttributes;
 
 	ApiError handlerError(WebRequest webRequest) {
-		@SuppressWarnings("deprecation")
-		Map<String, Object> attributes = this.errorAttributes.getErrorAttributes(webRequest, true);
+		Map<String, Object> attributes = this.errorAttributes.getErrorAttributes(webRequest, ErrorAttributeOptions.of(Include.MESSAGE,Include.BINDING_ERRORS));
 		String message = (String) attributes.get("message");
 		String path = (String) attributes.get("path");
 		int status = (Integer) attributes.get("status");
-		return new ApiError(status, message, path);
+
+		ApiError apiErr = new ApiError(status, message, path);
+		if (attributes.get("errors") != null) {
+			@SuppressWarnings("unchecked")
+			List<FieldError> fieldErrors = (List<FieldError>) attributes.get("errors");
+			Map<String, String> validationErrors = new HashMap<>();
+			for (FieldError fieldError : fieldErrors) {
+				validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+			}
+			apiErr.setValidationErrors(validationErrors);
+		}
+		return apiErr;
 	}
 
 	@Override
--- ws/src/main/java/com/tokay/ws/user/UserController.java
+++ ws/src/main/java/com/tokay/ws/user/UserController.java
@@ -31,17 +31,4 @@ public class UserController {
 		userService.addUser(user);
 		return new GenericResponse("user created");
 	}
-
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ApiError handleValidationExcepiton(MethodArgumentNotValidException exception) {
-		ApiError error = new ApiError(400, "Validation Error", "/api/1.0/users");
-		Map<String, String> validationErrors = new HashMap<>();
-		for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
-			validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
-		}
-		error.setValidationErrors(validationErrors);
-		return error;
-	}
-
 }
