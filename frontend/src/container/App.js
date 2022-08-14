7d4aaf744984fc6dd984bb2641b6a988efbab2ca
tokayOzgur <ozytky@gmail.com>
2022-08-14 21:55:41 +0300

    router

--- frontend/src/container/App.js
+++ frontend/src/container/App.js
@@ -1,15 +1,24 @@
 import React from "react";
-import ApiProgress from "../shared/ApiProgress";
 import LanguageSelector from '../components/LanguageSelector';
 import UserSingupPage from '../pages/UserSingupPage';
 import LoginPage from '../pages/LoginPage';
 import HomePage from "../pages/HomePage";
 import UserPage from "../pages/UserPage";
+import { HashRouter, Route, Redirect, Switch } from "react-router-dom";
+
+
 function App() {
   return (
-    <div className="row">
-      {/* <HomePage/> */}
-      <UserPage/>
+    <div>
+      <HashRouter>
+        <Switch>
+          <Route exact path={"/"} component={HomePage} />
+          <Route path={"/login"} component={LoginPage} />
+          <Route path={"/singup"} component={UserSingupPage} />
+          <Route path={"/user/:username"} component={UserPage} />
+          <Redirect to={"/"} />
+        </Switch>
+      </HashRouter>
       <LanguageSelector />
     </div>
   );
