f18182fa56a1ae98cfdf4cdbd381c2c94648ec3b
tokayOzgur <ozytky@gmail.com>
2022-08-14 20:58:56 +0300

    Sayfa ekleme

--- frontend/src/container/App.js
+++ frontend/src/container/App.js
@@ -3,15 +3,13 @@ import ApiProgress from "../shared/ApiProgress";
 import LanguageSelector from '../components/LanguageSelector';
 import UserSingupPage from '../pages/UserSingupPage';
 import LoginPage from '../pages/LoginPage';
+import HomePage from "../pages/HomePage";
+import UserPage from "../pages/UserPage";
 function App() {
   return (
     <div className="row">
-      <div className="col">
-          <LoginPage />
-      </div>
-      <div className="col">
-          <UserSingupPage />
-      </div>
+      {/* <HomePage/> */}
+      <UserPage/>
       <LanguageSelector />
     </div>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,12 +6,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './i18n';
 import App from './container/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
