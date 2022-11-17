a5782951e1748f36df015e835ddae9f914d186b8
tokayOzgur <ozytky@gmail.com>
2022-11-17 23:30:37 +0300

    hoax-submit-view

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import UserList from "../components/UserList";
+import HoaxSubmit from "../components/HoaxSubmit";
+import { useSelector } from "react-redux";
 
 const HomePage = () => {
+  const { isLoggedIn } = useSelector((store) => ({
+    isLoggedIn: store.isLoggedIn,
+  }));
   return (
     <div className="container">
-      <UserList />
+      <div className="row">
+        <div className="col-md-4 col-sm-12 mb-sm-3">
+          <UserList />
+        </div>
+        <div className="col-md-8 col-sm-12">{isLoggedIn && <HoaxSubmit />}</div>
+      </div>
     </div>
   );
 };
