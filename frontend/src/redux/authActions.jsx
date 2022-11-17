f8e5886dcf81c9bd9874855b9281a33582d9588f
tokayOzgur <ozytky@gmail.com>
2022-11-17 23:06:00 +0300

    reflecting-update-on-topbar

--- frontend/src/redux/authActions.jsx
+++ frontend/src/redux/authActions.jsx
@@ -1,33 +1,43 @@
-import * as ACTIONS from './Constants';
-import { login, signup } from '../api/apiCalls';
+import * as ACTIONS from "./Constants";
+import { login, signup } from "../api/apiCalls";
 
 export const logoutSuccess = () => {
   return {
-    type: ACTIONS.LOGOUT_SUCCESS
+    type: ACTIONS.LOGOUT_SUCCESS,
   };
 };
 
-export const loginSuccess = authState => {
+export const loginSuccess = (authState) => {
   return {
     type: ACTIONS.LOGIN_SUCCESS,
-    payload: authState
+    payload: authState,
   };
 };
 
-export const loginHandler = credentials => {
-  return async function(dispatch) {
+export const updateSuccess = ({ displayName, image }) => {
+  return {
+    type: ACTIONS.UPDATE_SUCCESS,
+    payload: {
+      displayName,
+      image,
+    },
+  };
+};
+
+export const loginHandler = (credentials) => {
+  return async function (dispatch) {
     const response = await login(credentials);
     const authState = {
       ...response.data,
-      password: credentials.password
+      password: credentials.password,
     };
     dispatch(loginSuccess(authState));
     return response;
   };
 };
 
-export const signupHandler = user => {
-  return async function(dispatch) {
+export const signupHandler = (user) => {
+  return async function (dispatch) {
     const response = await signup(user);
     await dispatch(loginHandler(user));
     return response;
