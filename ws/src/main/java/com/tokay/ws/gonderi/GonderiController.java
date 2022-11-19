51079d2bfa6a29bdacb27d9ece188699ebf9f17d
tokayOzgur <ozytky@gmail.com>
2022-11-19 15:19:19 +0300

    hoax-get-controller

--- ws/src/main/java/com/tokay/ws/gonderi/GonderiController.java
+++ ws/src/main/java/com/tokay/ws/gonderi/GonderiController.java
@@ -3,21 +3,31 @@ package com.tokay.ws.gonderi;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tokay.ws.shared.GenericResponse;
 
 @RestController
+@RequestMapping("/api/1.0")
 public class GonderiController {
 
 	@Autowired
 	GonderiService gonderiService;
 
-	@PostMapping("/api/1.0/gonderi")
+	@PostMapping("/gonderi")
 	GenericResponse saveGonderi(@Valid @RequestBody Gonderi gonderi) {
 		gonderiService.save(gonderi);
 		return new GenericResponse("Gonderi is saved.");
+	}
+
+	@GetMapping("/gonderiler")
+	Page<Gonderi> getGonderiler(Pageable page) {
+		return gonderiService.getGonderiler(page);
 	}
 }
--- ws/src/main/java/com/tokay/ws/gonderi/GonderiService.java
+++ ws/src/main/java/com/tokay/ws/gonderi/GonderiService.java
@@ -2,6 +2,8 @@ package com.tokay.ws.gonderi;
 
 import java.util.Date;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,5 +19,9 @@ public class GonderiService {
 		gonderi.setTimestamp(new Date());
 		gonderiRepository.save(gonderi);
 
+	}
+
+	public Page<Gonderi> getGonderiler(Pageable page) {
+		return gonderiRepository.findAll(page);
 	}
 }
