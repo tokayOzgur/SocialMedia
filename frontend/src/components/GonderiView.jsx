d059157c4ac80b1c2fba6ea40fd93e7a3670612b
tokayOzgur <ozytky@gmail.com>
2022-11-21 20:58:35 +0300

    gonderi-view-with-user

--- frontend/src/components/GonderiView.jsx
+++ frontend/src/components/GonderiView.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from "react";
+import ProfileImageWithDefault from './ProfileImageWithDefault';
+import { Link } from 'react-router-dom';
 
 const GonderiView = (props) => {
-    const { gonderi } = props;
-    return <div className="card p-1">{gonderi.content}</div>;
-}
+  const { gonderi } = props;
+  const { user, content } = gonderi;
+  const { username, displayName, image } = user;
+  return (
+    <div className="card p-1">
+      <div className="d-flex">
+        <ProfileImageWithDefault
+          image={image}
+          width="32"
+          height="32"
+          className="rounded-circle m-1"
+        />
+        <div className="flex-fill m-auto pl-2">
+          <Link to={`/user/${username}`} className="text-dark">
+            <h6>
+              {displayName}@{username}
+            </h6>
+          </Link>
+        </div>
+      </div>
+      <div className="pl-5">{content}</div>
+    </div>
+  );
+};
 
-export default GonderiView
+export default GonderiView;
